a3ad0850a2f0b3af003d6becfab542d3187b6287
katie <katieanriley@gmail.com>
2025-08-30 14:09:00 -0700

    create cart and line item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   has_one_attached :image
+  has_many :line_items
   after_commit -> { broadcast_refresh_later_to 'products' }
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   validate :acceptable_image
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -13,6 +15,15 @@ class Product < ApplicationRecord
     acceptable_types = [ 'image/gif', 'image/jpeg', 'image/png' ]
     unless acceptable_types.include?(image.content_type)
       errors.add(:image, 'must be a GIF, JPG, or PNG image')
+    end
+  end
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line items present')
+      throw :abort
     end
   end
 end
